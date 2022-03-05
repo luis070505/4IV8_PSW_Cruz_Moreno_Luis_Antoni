e24968961253e002cb9a30c51caa13a408a78a7d
luis070505 <cruz.moreno.luis.antonio3@gmail.com>
2022-03-05 09:38:39 -0600

    examen

--- Examen/JS/validacion.js
+++ Examen/JS/validacion.js
@@ -1,117 +1,78 @@
-function valinicial(elemento){
-    var numero = parseInt(elemento.value,10);
-    if(isNaN(numero)){
-        alert('Ingrese solo números.');
-        elemento.focus();
-        elemento.select();
-        return;
+function validar(ingresarDatos){
+
+    var checkOK = "1234567890";
+
+    var checkStr = ingresarDatos.precio.value;
+
+    var allValido = true;
+
+    for(var i = 0; i < checkStr.length; i++){
+        var ch = checkStr.charAt(i);
+        for(var j = 0; j < checkOK.length; j++){
+            if(ch == checkOK.charAt(j)){
+                break;
+            }
+        }
+        if(j == checkOK.length){
+            allValido = false;
+            break;
+        }
     }
-    if(numero<100000 || numero>1000000){
-        alert('Ingresar valor entre 100 mil y 1 millon');
-        elemento.focus();
+
+    if(!allValido){
+        alert("Escriba solamente numeros");
+        ingresarDatos.precio.focus();
         return false;
     }
-    return true;
-}
 
-function valenganche(elemento){
-    
-    var numero = parseInt(elemento.value,10);
-    if(isNaN(numero)){
-        alert('Ingrese solo números.');
-        elemento.focus();
-        elemento.select();
-        return;
+    var checkStr = ingresarDatos.cuotaIn.value;
+
+    var allValido = true;
+
+    for(var i = 0; i < checkStr.length; i++){
+        var ch = checkStr.charAt(i);
+        for(var j = 0; j < checkOK.length; j++){
+            if(ch == checkOK.charAt(j)){
+                break;
+            }
+        }
+        if(j == checkOK.length){
+            allValido = false;
+            break;
+        }
     }
 
-    if(numero<(document.getElementById("valaut").value)*0.1){
-        alert('Enganche Minimo del 10% del valor inicial');
-        elemento.focus();
+    if(!allValido){
+        alert("Escriba solamente numeros");
+        ingresarDatos.cuotaIn.focus();
         return false;
     }
-    return true;
+
 }
 
-document.getElementById("add").onclick = function () {
+const form = document.getElementById('ingresarDatos');
+if(allValido=true){
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    let ingresarDatosData  = new FormData(form);
+    let tablaAutoRef = document.getElementById('tablaAuto')
+    let newTransactionRowRef = tablaAutoRef.insertRow(-1);
+
+    let cellRef = newTransactionRowRef.insertCell(0);
+    cellRef.textContent= ingresarDatos.precio.value;
     
-    var valini = document.getElementById("valaut").value;
-    var enganche = document.getElementById("eng").value;
-    var periodo = document.querySelector("input[name=meses]:checked").value;
+    cellRef = newTransactionRowRef.insertCell(1);
+    cellRef.textContent= ingresarDatos.precio.value/ingresarDatos.cuotaIn.value;
+    
+    cellRef = newTransactionRowRef.insertCell(2);
+    cellRef.textContent= ingresarDatos.periodo.value;
+    
+    cellRef = newTransactionRowRef.insertCell(3);
+    cellRef.textContent= ingresarDatos.precio.value*0.0394;
 
-    if(valini == ""){
-        alert("Ingresar valor inicial");
-        return;
-    }
-
-    if(enganche == ""){
-        alert("Ingresar Enganche");
-        return;
-    }
-
-    var td_valini_node = document.createElement("td");
-    var valini_text_node = document.createTextNode(valini);
-    td_valini_node.appendChild(valini_text_node);
-
-    var td_enganche_node = document.createElement("td");
-    var enganche_text_node = document.createTextNode(enganche);
-    td_enganche_node.appendChild(enganche_text_node);
-
-    var td_finan_node = document.createElement("td");
-    var finan_text_node = document.createTextNode(valini - enganche);
-    td_finan_node.appendChild(finan_text_node);
-
-    var td_periodo_node = document.createElement("td");
-    var periodo_text_node = document.createTextNode(periodo);
-    td_periodo_node.appendChild(periodo_text_node);
-
-    var td_int_node = document.createElement("td");
-    var int_text_node = document.createTextNode('3.94%');
-    td_int_node.appendChild(int_text_node);
-
-    var cuot = (valini-enganche)*(1+(0.0394*periodo));
-    var fvalfut = cuot.toFixed(2);
-    var td_valfut_node = document.createElement("td");
-    var valfut_text_node = document.createTextNode('$'+fvalfut);
-    td_valfut_node.appendChild(valfut_text_node);
-
-    var fcuot = cuot/periodo;
-    fcuot = fcuot.toFixed(2);
-
-    var td_cuota_node = document.createElement("td");
-
-    var cuota_text_node = document.createTextNode('$'+fcuot);
-
-    td_cuota_node.appendChild(cuota_text_node);
-
-    var ele_a = document.createElement("a");
-
-    ele_a.setAttribute("href","javascript:void(0);");
-
-    ele_a.setAttribute("onclick","del_tr(this);");
-
-    var del_node = document.createTextNode("Eliminar");
-
-    ele_a.appendChild(del_node);
-
-    var td_del_node = document.createElement("td");
-
-    td_del_node.appendChild(ele_a);
-
-    var tr_node = document.createElement("tr");
-
-    tr_node.appendChild(td_valini_node);
-    tr_node.appendChild(td_enganche_node);
-    tr_node.appendChild(td_finan_node);
-    tr_node.appendChild(td_periodo_node);
-    tr_node.appendChild(td_int_node);
-    tr_node.appendChild(td_valfut_node);
-    tr_node.appendChild(td_cuota_node);
-    tr_node.appendChild(td_del_node);
-
-    var table_node = document.getElementsByTagName("table")[0];
-    table_node.appendChild(tr_node);
-}
-del_tr = function (obj) {
-    var table_node = obj.parentNode.parentNode.parentNode;
-    table_node.removeChild(obj.parentNode.parentNode);
+    cellRef = newTransactionRowRef.insertCell(4);
+    cellRef.textContent= (ingresarDatos.precio.value)*(1 + 0.0394)*(ingresarDatos.periodo.value);
+    
+})
 }
